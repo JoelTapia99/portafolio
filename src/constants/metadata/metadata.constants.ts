e31ccd2fc6a23ad3d1150fba58e4a32bc5dd630f
joeltapia99 <joelts159@gmail.com>
2025-10-10 17:19:55 -0500

    add metadata

---
+++ src/constants/metadata/metadata.constants.ts
@@ -0,0 +1,9 @@
+import {HOST_URL} from "../app.constants.ts";
+
+export const METADATA = {
+    tile: "Joel Tapia | Desarrollador de software",
+    description: "Joel Tapia, programador de software con experiencia en React, Angular, TypeScript, JavaScript, Java, Spring, PHP | Quito-Ecuador",
+    url: HOST_URL,
+    type: 'website',
+    image: 'https://joeltapia.com/images/og-image.jpg',
+}
