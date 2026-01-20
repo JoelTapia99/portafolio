bef72e929313d8410dcad29fa217d83242a9663a
JoelTapia99 <joelts159@gmail.com>
2026-01-20 00:48:47 -0500

    add Navbar.astro component

--- src/constants/metadata.constants.ts
+++ src/constants/metadata.constants.ts
@@ -1,9 +1,9 @@
 import { HOST_URL } from "./app.constants.ts";
 
 export const METADATA = {
-  tile: "Joel Tapia | Desarrollador de software",
+  tile: "Joel Tapia | Programador",
   description:
-    "Joel Tapia, programador de software con experiencia en React, Angular, TypeScript, JavaScript, Java, Spring, PHP | Quito-Ecuador",
+      "Portfolio de Joel Tapia - Programador Full Stack especializado en Angular, Laravel, ReactNative y más. Descubre mis proyectos, habilidades y experiencia en desarrollo web y móvil.",
   url: HOST_URL,
   type: "website",
   image: "https://joeltapia.com/images/og-image.jpg",
